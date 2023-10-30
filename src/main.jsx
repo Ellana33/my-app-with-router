85d18d2790fc7e92e0e40e65c0e2c6c3cdac4c74
Anaelle Cauvin <anaelle.cauvin@epitech.eu>
2023-10-30 11:32:52 +0100

    finish loader part

--- src/main.jsx
+++ src/main.jsx
@@ -5,13 +5,37 @@ import Home from "./pages/Home";
 import About from "./pages/About";
 import Article from "./pages/Article";
 
+const getSomeData = (id) => {
+  const allData = {
+    42: {
+      title: "Lorem Ipsum",
+      content: "Lorem ipsum dolor sit amet",
+    },
+    123: {
+      title: "Schnapsum",
+      content: "Lorem Elsass ipsum Salut bisamme",
+    },
+    666: {
+      title: "Cupcake Ipsum",
+      content: "Tiramisu pastry wafer brownie soufflé",
+    },
+  };
+
+  return allData[id];
+};
+
 const router = createBrowserRouter([
   {
     element: <App />,
+    loader: () => {
+      const weather = 'sunny';
+      return weather
+    },
+    id: "app",
     children: [
-      { path: "/", element: <Home /> },
+      { path: "/", element: <Home />},
       { path: "/about", element: <About /> },
-      { path: "/article/:id", element: <Article /> },
+      { path: "/article/:id", element: <Article />, loader: ({ params }) => { return getSomeData(params.id) }},
     ],
   },
 ]);
