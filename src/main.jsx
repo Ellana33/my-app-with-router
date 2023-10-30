ee96a62e72f5840c1ddd7b6ec7dd771c0c63472c
Anaelle Cauvin <anaelle.cauvin@epitech.eu>
2023-10-30 10:01:33 +0100

    finish workshop

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,21 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import ReactDOM from "react-dom/client";
+import { createBrowserRouter, Link, RouterProvider } from "react-router-dom";
+import App from "./App";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Article from "./pages/Article";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+const router = createBrowserRouter([
+  {
+    element: <App />,
+    children: [
+      { path: "/", element: <Home /> },
+      { path: "/about", element: <About /> },
+      { path: "/article/:id", element: <Article /> },
+    ],
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <RouterProvider router={router} />
+);
